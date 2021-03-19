a84a811504f48ffa1a3464ad20b1dc70a71d7ee2
mehmet <mehmetkutlu35.mtk@gmail.com>
2021-03-19 16:38:18 +0300

    LayoutSouthParkApp

---
+++ app/src/main/java/com/mehmettolgakutlu/layoutsouthpark/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mehmettolgakutlu.layoutsouthpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
